aa0353128d37bf82812b6b105333d5de99a6bfce
sagnikc395 <sagnikchatterjee607@gmail.com>
2024-12-17 12:34:32 +0530

    src: build methods and interfaces

---
+++ src/store/Store.ts
@@ -0,0 +1,22 @@
+//in-memory store and whenver we flip it out for redis
+// we can create a same shape for this with the given functionality
+
+export abstract class Store {
+  constructor() {}
+
+  initRoom() {}
+
+  getChats(room: string, limit: number, offset: number) {}
+
+  addChat(room: string, limit: number, offset: number) {}
+  upvote(room: string, chatId: string) {}
+}
+
+type UserId = string;
+interface Chat {
+  //shape of our chat obj
+  userId: UserId;
+  name: string;
+  message: string;
+  upvotes: UserId[]; // who has upvoted what 
+}
